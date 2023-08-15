4fe8d93b9d80b66f672746e2aa0dab287b02aec2
R30 <r30@R30s-Mac-mini.local>
2023-08-15 09:45:27 +0530

    error solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:audio_player/view/Home.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(useMaterial3: true),
-    routes: {
-      '/': (context) => Home(),
-    },
-  ));
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      routes: {
+        '/': (context) => Home(),
+      },
+    ),
+  );
 }
-
